362b8e8a9f98e79f44c6995a5c863df8840f974a
Shamanth <shamanthm7777@gmail.com>
2026-05-23 00:26:22 +0530

    modified

--- sample-app/index.js
+++ sample-app/index.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
     <body>
       <div class="card">
         <h1>🚀 Sample App</h1>
-        <p>Sample App Successfully Deployed version 3</p>
+        <p>Deploying using docker</p>
         <span class="version">v1.1.2</span>
       </div>
     </body>
